713199905a5b28c400909dba08066ee218fa4943
Neeraj Kashyap <neeraj@bugout.dev>
2023-10-25 00:18:20 -0700

    Started defining data structures and game state

--- src/LightningAndSmoke.sol
+++ src/LightningAndSmoke.sol
@@ -2,6 +2,11 @@
 
 pragma solidity ^0.8.19;
 
+import {IERC721} from "lib/openzeppelin-contracts/contracts/token/ERC721/IERC721.sol";
+import {StatBlockBase} from "lib/web3/contracts/stats/StatBlock.sol";
+
+import {PlayerType, PitchType, SwingType, VerticalLocation, HorizontalLocation, Pitch, Swing, Session} from "./data.sol";
+
 /*
 LightningAndSmoke implements a simple game in which two NFTs (from any ERC721 contracts) complete against
 each other in a single pitch of baseball. One NFT acts as the pitcher, the other one acts as the batter,
@@ -23,8 +28,8 @@ Functionality:
       can have a different price than joining an existing session. In general, we will keep it cheaper
       to start a session than to join a sesion that someone else started -- this will incentivize many
       matches. The cost will disincentivize bots grinding against themselves. The contract is deployed
-      with `sessionStartPrice`, `sessionJoinPrice`, and `treasuryAddress` parameters. Prices are
-      transferred to the `treasuryAddress` when a session is either started or joined.
+      with `feeTokenAddress`, `sessionStartPrice`, `sessionJoinPrice`, and `treasuryAddress` parameters.
+      Prices are transferred to the `treasuryAddress` when a session is either started or joined.
 - [ ] Once a session starts, both the pitcher and the batter can commit their moves.
 - [ ] Commitments are signed EIP712 messages representing the moves.
 - [ ] LightningAndSmoke contract is deployed with a `blocksPerPhase` parameter. If one player commits
@@ -43,6 +48,44 @@ Functionality:
 - [ ] If neither player reveals their move before `blocksPerPhase` blocks have passed since the
       second commit, then the session is cancelled and both players may unstake their NFTs.
  */
-contract LightningAndSmoke {
+contract LightningAndSmoke is StatBlockBase {
+    uint256 public FeeTokenAddress;
+    uint256 public SessionStartPrice;
+    uint256 public SessionJoinPrice;
+    uint256 public TreasuryAddress;
 
+    uint256 public NumSessions;
+
+    // Session ID => session state
+    // NOTE: Sessions are 1-indexed
+    mapping(uint256 => Session) public SessionState;
+
+    // ERC721 address => ERC721 token ID => address of player who staked that character into the Lightning and Smoke contract
+    mapping(address => mapping(uint256 => address)) public Staker;
+
+    // ERC721 address => ERC721 token ID => session that that character is staked into
+    // NOTE: Sessions are 1-indexed
+    mapping(address => mapping(uint256 => uint256)) public StakedSession;
+
+    event SessionStarted(uint256 sessionID, address indexed nftAddress, uint256 indexed tokenID, PlayerType indexed role);
+
+    constructor(address feeTokenAddress, uint256 sessionStartPrice, uint256 sessionJoinPrice, address treasuryAddress) {
+        FeeTokenAddress = feeTokenAddress;
+        SessionStartPrice = sessionStartPrice;
+        SessionJoinPrice = sessionJoinPrice;
+        TreasuryAddress = treasuryAddress;
+    }
+
+    // LightningAndSmoke is an autnonomous game, and so the only administrator for NFT stats is the
+    // LightningAndSmoke contract itself.
+    function isAdministrator(
+        address account
+    ) public view virtual override returns (bool) {
+        return account == address(this);
+    }
+
+    // Emits:
+    // - SessionStarted
+    function startSession(address nftAddress, uint256 tokenID, PlayerType role) public virtual returns (uint256) {
+    }
 }
---
+++ src/data.sol
@@ -0,0 +1,120 @@
+// SPDX-License-Identifier: Apache-2.0
+
+pragma solidity ^0.8.19;
+
+/*
+This file defines the basic data types and structures that are used in Lightning and Smoke game mechanics.
+*/
+
+/*
+Player types:
+- Pitcher - 0
+- Batther - 1
+*/
+enum PlayerType {
+    Pitcher,
+    Batter
+}
+
+/*
+Pitch types:
+- Fastball - 0
+- Curveball - 1
+- Changeup - 2
+- Knuckleball - 3
+*/
+enum PitchType {
+    Fastball,
+    Curveball,
+    Changeup,
+    Knuckleball
+}
+
+/*
+Types of swings a batter can make:
+- Contact - 0
+- Power - 1
+- Bunt - 2
+- Check - 3
+*/
+enum SwingType {
+    Contact,
+    Power,
+    Bunt,
+    Soft
+}
+
+/*
+Possible vertical locations for a pitch:
+- HighBall - 0
+- HighStrike - 1
+- Middle - 2
+- LowStrike - 3
+- LowBall - 4
+- Dirt - 5
+*/
+enum VerticalLocation {
+    HighBall,
+    HighStrike,
+    Middle,
+    LowStrike,
+    LowBall
+}
+
+/*
+Possible horizontal locations for a pitch:
+- InsideBall - 0
+- InsideStrike - 1
+- Middle - 2
+- OutsideStrike - 3
+- OutsideBall - 4
+*/
+enum HorizontalLocation {
+    InsideBall,
+    InsideStrike,
+    Middle,
+    OutsideStrike,
+    OutsideBall
+}
+
+/*
+Pitch represents the pitcher's move in a Lightning and Smoke session.
+*/
+struct Pitch {
+    uint256 nonce;
+    PitchType kind;
+    VerticalLocation vertical;
+    HorizontalLocation horizontal;
+}
+
+/*
+Swing represents the batter's move in a Lightning and Smoke session.
+*/
+struct Swing {
+    uint256 nonce;
+    SwingType kind;
+    VerticalLocation vertical;
+    HorizontalLocation horizontal;
+}
+
+/*
+Session represents the state of a Lightning and Smoke session.
+*/
+struct Session {
+    uint256 startBlock;
+    uint256 commitmentsBlock;
+
+    address pitcherAddress;
+    uint256 pitcherTokenID;
+    bool didPitcherCommit;
+    bool didPitcherReveal;
+    bytes pitcherCommit;
+    Pitch pitcherReveal;
+
+    address batterAddress;
+    uint256 batterTokenID;
+    bool didBatterCommit;
+    bool didBatterReveal;
+    bytes batterCommit;
+    Swing batterReveal;
+}
