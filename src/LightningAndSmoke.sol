be376000ea54e93952595f02094bd816238f8666
Neeraj Kashyap <neeraj@bugout.dev>
2023-10-24 22:49:17 -0700

    Some fundamental principles of LightningAndSmoke

---
+++ src/LightningAndSmoke.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: Apache-2.0
+
+pragma solidity ^0.8.19;
+
+/*
+LightningAndSmoke implements a simple game in which two NFTs (from any ERC721 contracts) complete against
+each other in a single pitch of baseball. One NFT acts as the pitcher, the other one acts as the batter,
+and they face off with the game tied, the bases loaded, a full count, and two outs in the bottom of the 9th
+inning.
+
+The ball will not go foul. Either the pitcher gets the batter out or the batter wins the game.
+
+Authors: Moonstream (https://moonstream.to)
+
+Functionality:
+- [ ] Game is played by NFTs from *any* collection.
+- [ ] All NFTs start with stats of 0 and improve their stats by playing sessions of Lightning and Smoke.
+- [ ] Player can start a session as pitcher or batter.
+- [ ] Player can stake into existing session as pitcher or batter - complement of the role that was staked
+      to start the session.
+- [ ] When a pitcher and batter are staked into a session, the session automatically starts.
+- [ ] Staking a character into a session costs either native tokens or ERC20 tokens. Starting a session
+      can have a different price than joining an existing session. In general, we will keep it cheaper
+      to start a session than to join a sesion that someone else started -- this will incentivize many
+      matches. The cost will disincentivize bots grinding against themselves. The contract is deployed
+      with `sessionStartPrice`, `sessionJoinPrice`, and `treasuryAddress` parameters. Prices are
+      transferred to the `treasuryAddress` when a session is either started or joined.
+- [ ] Once a session starts, both the pitcher and the batter can commit their moves.
+- [ ] Commitments are signed EIP712 messages representing the moves.
+- [ ] LightningAndSmoke contract is deployed with a `blocksPerPhase` parameter. If one player commits
+      their move but the other one does not commit their move before `blocksPerPhase` blocks have
+      elapsed since the session started, then the first player wins by forfeit. They can submit a
+      transaction to end the session, unstake their NFT, and earn their reward.
+- [ ] A player can unstake their NFT from a session if the session has either not started or if the session
+      has completed.
+- [ ] If both players commit their moves before `blocksPerPhase` blocks have elapsed since the session
+      started, then the session enters the "reveal" phase. In this phase, each player has `blocksPerPhase`
+      blocks to reveal their moves. They do this by submitting the EIP712 messages that were signed
+      to generate their commits. The session is resolved once the second player reveals their move (in the
+      same transaction).
+- [ ] If one player reveals their move before `blocksPerPhase` blocks have passed since the second
+      commit but the other one doesn't, then the player who revealed wins by default.
+- [ ] If neither player reveals their move before `blocksPerPhase` blocks have passed since the
+      second commit, then the session is cancelled and both players may unstake their NFTs.
+ */
+contract LightningAndSmoke {
+
+}
