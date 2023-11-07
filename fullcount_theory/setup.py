af44b29a3c2b14f4cd7c6ba1aba42f9cc29efed5
Neeraj Kashyap <nkashy1@gmail.com>
2023-11-07 09:09:51 -0800

    Updated setup.py - added eth-brownie and moonworm dependencies

--- fullcount_theory/setup.py
+++ fullcount_theory/setup.py
@@ -1,3 +1,4 @@
+import os
 from setuptools import find_packages, setup
 
 with open("fullcount_theory/version.txt") as ifp:
@@ -7,13 +8,15 @@ long_description = ""
 with open("README.md") as ifp:
     long_description = ifp.read()
 
+os.environ["BROWNIE_LIB"] = "1"
+
 setup(
     name="fullcount_theory",
     version=VERSION,
     packages=find_packages(),
-    install_requires=[],
+    install_requires=["eth-brownie"],
     extras_require={
-        "dev": ["black"],
+        "dev": ["black", "moonworm[moonstream]"],
         "distribute": ["setuptools", "twine", "wheel"],
     },
     description="Fullcount.xyz game design tool",
