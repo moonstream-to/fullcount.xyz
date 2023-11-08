c15489f5f8d9b26355b7b538e67b9b103aa54aaf
Anton Mushnin <anton.mushnin@gmail.com>
2023-11-08 16:30:58 +0300

    displaying tokens

---
+++ src/components/CharacterCard.tsx
@@ -0,0 +1,25 @@
+import { Token } from "./OwnedTokens";
+import { Flex, Image, Text } from "@chakra-ui/react";
+import styles from "./CharacterCard.module.css";
+import globalStyles from "./OwnedTokens.module.css";
+import { useGameContext } from "../contexts/GameContext";
+
+const CharacterCard = ({ token }: { token: Token }) => {
+  const { updateContext } = useGameContext();
+  return (
+    <Flex className={styles.container}>
+      <Image h={"137px"} w={"137px"} alt={token.name} src={token.image} />
+      <Flex className={styles.bottom}>
+        <Text>{token.name}</Text>
+        <button
+          className={globalStyles.button}
+          onClick={() => updateContext({ selectedToken: token.id })}
+        >
+          Play
+        </button>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default CharacterCard;
--- src/components/OwnedTokens.tsx
+++ src/components/OwnedTokens.tsx
@@ -7,11 +7,19 @@ import { useContext } from "react";
 import { useGameContext } from "../contexts/GameContext";
 import useMoonToast from "../hooks/useMoonToast";
 import CreateNewCharacter from "./CreateNewCharacter";
+import queryCacheProps from "../hooks/hookCommon";
+import CharacterCard from "./CharacterCard";
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const FullcountABI = require("../web3/abi/FullcountABI.json");
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const tokenABI = require("../web3/abi/BLBABI.json");
+
+export interface Token {
+  id: number;
+  name: string;
+  image: string;
+}
 
 const OwnedTokens = () => {
   const web3ctx = useContext(Web3Context);
@@ -19,6 +27,9 @@ const OwnedTokens = () => {
   const queryClient = useQueryClient();
   const toast = useMoonToast();
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const tokenContract = new web3ctx.web3.eth.Contract(tokenABI) as any;
+  tokenContract.options.address = tokenAddress;
+
   const mintToken = useMutation(
     async ({ name, imageIndex }: { name: string; imageIndex: number }) => {
       if (!web3ctx.account) {
@@ -26,9 +37,6 @@ const OwnedTokens = () => {
           reject(new Error(`Account address isn't set`));
         });
       }
-      const tokenContract = new web3ctx.web3.eth.Contract(tokenABI) as any;
-
-      tokenContract.options.address = tokenAddress;
       return tokenContract.methods.mint(name, imageIndex).send({
         from: web3ctx.account,
         maxPriorityFeePerGas: null,
@@ -37,7 +45,7 @@ const OwnedTokens = () => {
     },
     {
       onSuccess: () => {
-        queryClient.invalidateQueries("contract_approval");
+        queryClient.invalidateQueries("owned_tokens");
       },
       onError: (e: Error) => {
         toast("Minting failed." + e?.message, "error");
@@ -45,9 +53,59 @@ const OwnedTokens = () => {
     },
   );
 
+  function decodeBase64Json(encodedData: string): any {
+    try {
+      // Split the encoded data to remove the data URI scheme if present
+      const base64String = encodedData.split(",")[1] || encodedData;
+
+      // Decode the base64 string to a UTF-8 string
+      const decodedStr = Buffer.from(base64String, "base64").toString("utf-8");
+
+      // Parse the JSON string to an object
+      return JSON.parse(decodedStr);
+    } catch (error) {
+      console.error("Failed to decode base64 JSON data:", error);
+      return null;
+    }
+  }
+
+  const ownedTokens = useQuery<Token[]>(
+    ["owned_tokens", web3ctx.account, web3ctx.chainId],
+    async () => {
+      console.log("ownedTokens");
+
+      const balanceOf = await tokenContract.methods.balanceOf(web3ctx.account).call();
+      const tokens = [];
+      for (let i = 0; i < balanceOf; i++) {
+        const tokenId = await tokenContract.methods.tokenOfOwnerByIndex(web3ctx.account, i).call();
+        const tokenMetadata = decodeBase64Json(
+          await tokenContract.methods.tokenURI(tokenId).call(),
+        );
+        console.log(tokenMetadata);
+        tokens.push({
+          id: tokenId,
+          name: tokenMetadata.name.split(` - ${tokenId}`)[0],
+          image: tokenMetadata.image,
+        });
+      }
+      console.log(tokens);
+      return tokens;
+    },
+    {
+      ...queryCacheProps,
+    },
+  );
+
   return (
     <Flex className={styles.container}>
       <Text className={styles.title}>Your NFTs</Text>
+      <Flex className={styles.cards}>
+        {ownedTokens.data &&
+          ownedTokens.data.map((token: Token, idx: number) => (
+            <CharacterCard token={token} key={idx} />
+          ))}
+      </Flex>
+
       <button className={styles.button} onClick={onOpen}>
         {mintToken.isLoading ? <Spinner /> : "Mint"}
       </button>
