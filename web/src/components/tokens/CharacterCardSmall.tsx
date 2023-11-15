60341dff3cea736d7320a9bb5e21a586ca9362a1
Anton Mushnin <anton.mushnin@gmail.com>
2023-11-15 18:31:09 +0300

    own card design

--- web/src/components/tokens/CharacterCardSmall.tsx
+++ web/src/components/tokens/CharacterCardSmall.tsx
@@ -1,19 +1,19 @@
-import { Flex, Image, Text } from "@chakra-ui/react";
+import { Box, Flex, Image, Text } from "@chakra-ui/react";
 import styles from "./CharacterCard.module.css";
-import globalStyles from "./OwnedTokens.module.css";
 import { useGameContext } from "../../contexts/GameContext";
 import { Session, Token } from "../../types";
-import { ReactNode, useEffect } from "react";
 
 const CharacterCard = ({
   token,
   session,
   isClickable = false,
+  isOwned,
   ...props
 }: {
   token: Token | undefined;
   session?: Session;
   isClickable?: boolean;
+  isOwned: boolean;
   [x: string]: any;
 }) => {
   const { updateContext } = useGameContext();
@@ -41,7 +41,8 @@ const CharacterCard = ({
       }}
       cursor={isClickable ? "pointer" : "default"}
     >
-      <Image h={"40px"} w={"40px"} alt={""} src={token.image} />
+      {isOwned && <Box w={"3px"} h={"48px"} bg={"white"} mr={"-6px"} />}
+      <Image h={"40px"} w={"40px"} alt={""} src={token.image} border="1px solid #4D4D4D" />
       <Flex direction={"column"} fontSize={"14px"}>
         <Text>{token.name}</Text>
         <Text>{`owner ${token.staker?.slice(0, 6)}...${token.staker?.slice(-4)}`}</Text>
