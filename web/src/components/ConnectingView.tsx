2fe272f8ab140960d65edcdaf8025be667bad4f5
Anton Mushnin <anton.mushnin@gmail.com>
2023-11-16 20:47:09 +0300

    adding Wyrm to Metamask

--- web/src/components/ConnectingView.tsx
+++ web/src/components/ConnectingView.tsx
@@ -1,14 +1,14 @@
 import styles from "./ConnectingView.module.css";
 import globalStyles from "./tokens/OwnedTokens.module.css";
-import { Flex, Text } from "@chakra-ui/react";
-import { useContext, useEffect } from "react";
+import { Flex, Spinner, Text } from "@chakra-ui/react";
+import { useContext, useEffect, useState } from "react";
 import Web3Context from "../contexts/Web3Context/context";
-import { supportedChains } from "../types";
 import { useGameContext } from "../contexts/GameContext";
 import { chainByChainId } from "../contexts/Web3Context";
 
 const ConnectingView = ({ nextStep }: { nextStep: () => void }) => {
   const web3Provider = useContext(Web3Context);
+  const [isSwitching, setIsSwitching] = useState(false);
   const { chainId } = useGameContext();
   useEffect(() => {
     if (web3Provider.buttonText === "Connected" && web3Provider.chainId === chainId) {
@@ -16,6 +16,38 @@ const ConnectingView = ({ nextStep }: { nextStep: () => void }) => {
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [web3Provider.buttonText, web3Provider.chainId]);
+
+  const switchToWyrm = async () => {
+    const wyrmID = 322;
+    const hexString = `0x${wyrmID.toString(16)}`;
+    setIsSwitching(true);
+    try {
+      await window.ethereum.request({
+        method: "wallet_switchEthereumChain",
+        params: [{ chainId: hexString }],
+      });
+    } catch (switchError: any) {
+      if (switchError.code === 4902) {
+        try {
+          await window.ethereum.request({
+            method: "wallet_addEthereumChain",
+            params: [
+              {
+                chainId: hexString,
+                chainName: "Wyrm",
+                rpcUrls: ["https://wyrm.constellationchain.xyz/http"],
+                blockExplorerUrls: ["https://wyrm.constellationchain.xyz/http"],
+                nativeCurrency: { decimals: 18, name: "cMATIC", symbol: "cMATIC" },
+              },
+            ],
+          });
+        } catch (addError) {
+          console.log(addError);
+        }
+      }
+    }
+    setIsSwitching(false);
+  };
 
   return (
     <Flex className={styles.container}>
@@ -26,16 +58,20 @@ const ConnectingView = ({ nextStep }: { nextStep: () => void }) => {
         with.
       </Text>
       {web3Provider.buttonText !== "Connected" && (
-        <button className={globalStyles.button} onClick={web3Provider.onConnectWalletClick}>
+        <button
+          className={globalStyles.connectionButton}
+          onClick={web3Provider.onConnectWalletClick}
+        >
           Connect with Metamask
         </button>
       )}
       {web3Provider.buttonText === "Connected" && (
-        <button
-          className={globalStyles.button}
-          onClick={() => web3Provider.changeChain(chainByChainId(chainId) as supportedChains)}
-        >
-          {`Switch to ${chainByChainId(chainId) ?? `chain #${chainId}`}`}
+        <button className={globalStyles.connectionButton} onClick={switchToWyrm}>
+          {isSwitching ? (
+            <Spinner h={"14px"} w={"14px"} />
+          ) : (
+            <Text>{`Switch to ${chainByChainId(chainId) ?? `chain #${chainId}`}`}</Text>
+          )}
         </button>
       )}
     </Flex>
