d3d959cfbafc5e63d54422ff867bd66e1b8fae48
Anton Mushnin <anton.mushnin@gmail.com>
2023-11-22 15:43:37 +0300

    fixing inviteView glitches

--- web/src/components/sessions/InviteView.tsx
+++ web/src/components/sessions/InviteView.tsx
@@ -3,18 +3,27 @@ import styles from "./InviteView.module.css";
 import globalStyles from "../GlobalStyles.module.css";
 import OwnedTokens from "../tokens/OwnedTokens";
 import { useGameContext } from "../../contexts/GameContext";
+import { useEffect } from "react";
 
 const InviteView = ({ isOpen, onClose }: { isOpen: boolean; onClose: () => void }) => {
   const { sessions, invitedBy, invitedTo, selectedSession } = useGameContext();
+  useEffect(() => {
+    console.log(sessions, invitedTo);
+    console.log(sessions?.length !== 0);
+    console.log(!sessions?.find((s) => s.sessionID === invitedTo));
+    console.log(sessions?.find((s) => s.sessionID === invitedTo)?.progress !== 2);
+  }, []);
 
   return (
     <>
       <Modal isOpen={isOpen || !!selectedSession} onClose={onClose}>
         <ModalContent className={styles.container} bg="#1A" minW={"662px"} my={"auto"}>
-          {sessions?.length !== 0 && !sessions?.find((s) => s.sessionID === invitedTo) && (
-            <Text>Can&apos;t find the session</Text>
-          )}
-          {sessions?.find((s) => s.sessionID === invitedTo)?.progress !== 2 && (
+          {sessions &&
+            sessions?.length !== 0 &&
+            !sessions?.find((s) => s.sessionID === invitedTo) && (
+              <Text>Can&apos;t find the session</Text>
+            )}
+          {sessions && sessions?.find((s) => s.sessionID === invitedTo)?.progress !== 2 && (
             <Text>Invitation is expired</Text>
           )}
           <Text className={styles.message}>
