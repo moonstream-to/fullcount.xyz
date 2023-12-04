3d38b3a3f28cd4cd4e237c049d8f082bbaac2868
Anton Mushnin <anton.mushnin@gmail.com>
2023-12-04 12:19:06 +0300

    joining flow for staked or not selected token

---
+++ web/src/components/sessions/SelectToken.tsx
@@ -0,0 +1,25 @@
+import { OwnedToken } from "../../types";
+import { Modal, ModalContent, Text } from "@chakra-ui/react";
+import styles from "./InviteView.module.css";
+import globalStyles from "../GlobalStyles.module.css";
+import OwnedTokens from "../tokens/OwnedTokens";
+
+const SelectToken = ({ isOpen, onClose }: { isOpen: boolean; onClose: () => void }) => {
+  return (
+    <>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalContent className={styles.container} bg="#1A" minW={"662px"} my={"auto"}>
+          <Text className={styles.message}>
+            Bottom of the ninth. <br />
+            Bases loaded. Full count.
+          </Text>
+          <Text className={globalStyles.gradientText}>Your move</Text>
+          <Text className={styles.invitationText}>{`Select character`}</Text>
+          <OwnedTokens forJoin={true} />
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default SelectToken;
