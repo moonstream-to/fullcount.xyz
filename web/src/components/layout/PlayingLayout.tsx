31268e97175d520e804f037f118c1e5b2a7ebfc0
Anton Mushnin <anton.mushnin@gmail.com>
2023-12-19 17:59:00 +0300

    lint

--- web/src/components/layout/PlayingLayout.tsx
+++ web/src/components/layout/PlayingLayout.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from "react";
-import Navbar from "./Navbar";
 import { Flex } from "@chakra-ui/react";
+
+import Navbar from "./Navbar";
 import { FULLCOUNT_ASSETS_PATH } from "../../constants";
 
 const sounds = {
@@ -24,7 +25,6 @@ const PlayingLayout = ({ children }: { children: ReactNode }) => {
       maxW={"1440px"}
       placeSelf={"center"}
       w={"100%"}
-      // bg={"#111133"}
     >
       <Navbar />
       <audio id="heartbeat" src={sounds.heartbeat} preload={"auto"} />
@@ -35,7 +35,6 @@ const PlayingLayout = ({ children }: { children: ReactNode }) => {
       <audio id="clapping" src={sounds.clapping} preload={"auto"} />
       <audio id="catch" src={sounds.catch} preload={"auto"} />
       <audio id="selectSound" src={sounds.select} preload={"auto"}></audio>
-
       {children}
     </Flex>
   );
