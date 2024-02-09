2e6144433dbd201dea6be2bed5a7faff5615da42
Anton Mushnin <anton.mushnin@gmail.com>
2024-02-09 11:01:42 +0300

    second two blocks

--- web/src/components/landing/Landing.tsx
+++ web/src/components/landing/Landing.tsx
@@ -2,11 +2,15 @@ import { Flex } from "@chakra-ui/react";
 import styles from "./Landing.module.css";
 import Hero from "./Hero";
 import UpcomingEvents from "./UpcomingEvents";
+import ChooseChalenge from "./ChooseChalenge";
+import LaunchCareer from "./LaunchCareer";
 const Landing = () => {
   return (
     <Flex w={"100%"} direction={"column"}>
       <Hero />
       <UpcomingEvents />
+      <ChooseChalenge />
+      <LaunchCareer />
     </Flex>
   );
 };
