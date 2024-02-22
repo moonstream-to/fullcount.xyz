07e605e2cc19d1614fd0ab77ab03810d31682221
Anton Mushnin <anton.mushnin@gmail.com>
2024-02-22 17:36:38 +0300

    minting FullcountPlayerOption

--- web/src/components/tokens/CreateNewCharacter.tsx
+++ web/src/components/tokens/CreateNewCharacter.tsx
@@ -2,6 +2,7 @@ import { Flex, Modal, ModalContent, Text, Image } from "@chakra-ui/react";
 import styles from "./CreateNewCharacter.module.css";
 import { useEffect, useState } from "react";
 import { TokenSource } from "../../types";
+import useUser from "../../contexts/UserContext";
 const NUMBER_OF_IMAGES = 24;
 
 const images: number[] = [];
@@ -21,11 +22,20 @@ const CreateNewCharacter = ({
   const [name, setName] = useState("");
   const [imageIndex, setImageIndex] = useState(-1);
   const [source, setSource] = useState<TokenSource>("FullcountPlayerAPI");
+  const { user } = useUser();
 
   useEffect(() => {
     setName("");
     setImageIndex(-1);
   }, [isOpen]);
+
+  useEffect(() => {
+    if (!user) {
+      setSource("BLBContract");
+    } else {
+      setSource("FullcountPlayerAPI");
+    }
+  }, [user]);
 
   const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === "Enter") {
@@ -36,26 +46,28 @@ const CreateNewCharacter = ({
     <Modal isOpen={isOpen} onClose={onClose}>
       <ModalContent className={styles.container} bg="#1A1D22" minW={{ base: "", lg: "782px" }}>
         <Text className={styles.message}>Create character</Text>
-        <Flex gap={"15px"}>
-          <Flex
-            onClick={() => setSource("FullcountPlayerAPI")}
-            p={"5px 40px"}
-            cursor={"pointer"}
-            border={`1px solid ${source === "FullcountPlayerAPI" ? "white" : "#333"}`}
-            color={`${source === "FullcountPlayerAPI" ? "white" : "#888"}`}
-          >
-            Fullcount Player
+        {user && (
+          <Flex gap={"15px"}>
+            <Flex
+              onClick={() => setSource("FullcountPlayerAPI")}
+              p={"5px 40px"}
+              cursor={"pointer"}
+              border={`1px solid ${source === "FullcountPlayerAPI" ? "white" : "#333"}`}
+              color={`${source === "FullcountPlayerAPI" ? "white" : "#888"}`}
+            >
+              Fullcount Player
+            </Flex>
+            <Flex
+              onClick={() => setSource("BLBContract")}
+              p={"5px 40px"}
+              cursor={"pointer"}
+              border={`1px solid ${source === "BLBContract" ? "white" : "#333"}`}
+              color={`${source === "BLBContract" ? "white" : "#888"}`}
+            >
+              BLB token
+            </Flex>
           </Flex>
-          <Flex
-            onClick={() => setSource("BLBContract")}
-            p={"5px 40px"}
-            cursor={"pointer"}
-            border={`1px solid ${source === "BLBContract" ? "white" : "#333"}`}
-            color={`${source === "BLBContract" ? "white" : "#888"}`}
-          >
-            BLB token
-          </Flex>
-        </Flex>
+        )}
         <Flex wrap={"wrap"} gap={"20px"} maxW={"800px"} placeSelf={"center"}>
           {images.map((_, idx: number) => (
             <Image
@@ -94,7 +106,6 @@ const CreateNewCharacter = ({
             Create
           </button>
         </Flex>
-        <button>{imageIndex}</button>
       </ModalContent>
     </Modal>
   );
