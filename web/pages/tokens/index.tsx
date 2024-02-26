e657dd49b8c4bae0786b22c939afbace1e575328
Anton Mushnin <anton.mushnin@gmail.com>
2024-02-26 14:00:26 +0300

    exit from managing tokens

--- web/pages/tokens/index.tsx
+++ web/pages/tokens/index.tsx
@@ -1,13 +1,25 @@
 import Layout from "../../src/components/layout/layout";
 import PlayingLayout from "../../src/components/layout/PlayingLayout";
 import TokenTransfers from "../../src/components/tokens/TokenTransfers";
-import { Flex } from "@chakra-ui/react";
+import { Flex, Image } from "@chakra-ui/react";
+import { FULLCOUNT_ASSETS_PATH } from "../../src/constants";
+import { useRouter } from "next/router";
 
 const Home = () => {
+  const router = useRouter();
   return (
     <Layout home={true} title="Fullcount - your tokens">
       <PlayingLayout>
-        <Flex justifyContent={"center"}>
+        <Flex alignItems={"center"} direction={"column"}>
+          <Image
+            alt="exit"
+            src={`${FULLCOUNT_ASSETS_PATH}/icons/exit.svg`}
+            h={"20px"}
+            w={"20px"}
+            cursor={"pointer"}
+            marginLeft={"auto"}
+            onClick={() => router.push("/", undefined, { shallow: true })}
+          />
           <TokenTransfers />
         </Flex>
       </PlayingLayout>
