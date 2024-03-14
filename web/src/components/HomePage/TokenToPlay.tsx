ee88151ba7579d9cdec7c877320cb9c9b35d7c05
Anton Mushnin <anton.mushnin@gmail.com>
2024-03-14 12:42:59 +0300

    errors handling

--- web/src/components/HomePage/TokenToPlay.tsx
+++ web/src/components/HomePage/TokenToPlay.tsx
@@ -21,14 +21,19 @@ const TokenToPlay = ({
         return;
       }
       const API_URL = "https://api.fullcount.xyz/pitch_distribution";
-      const res = await axios.get(`${API_URL}/${token.address}/${token.id}`);
-      const counts = new Array(25).fill(0);
-      res.data.pitch_distribution.forEach(
-        (l: PitchLocation) => (counts[l.pitch_vertical * 5 + l.pitch_horizontal] = l.count),
-      );
-      const total = counts.reduce((acc, value) => acc + value);
-      const rates = counts.map((value) => value / total);
-      return { rates, counts };
+      try {
+        const res = await axios.get(`${API_URL}/${token.address}/${token.id}`);
+        const counts = new Array(25).fill(0);
+        res.data.pitch_distribution.forEach(
+          (l: PitchLocation) => (counts[l.pitch_vertical * 5 + l.pitch_horizontal] = l.count),
+        );
+        const total = counts.reduce((acc, value) => acc + value);
+        const rates = counts.map((value) => value / total);
+        return { rates, counts };
+      } catch (e) {
+        console.log({ token, e });
+        return;
+      }
     },
     {
       enabled: !!token && isPitcher,
@@ -42,17 +47,22 @@ const TokenToPlay = ({
         return;
       }
       const API_URL = "https://api.fullcount.xyz/swing_distribution";
-      const res = await axios.get(`${API_URL}/${token.address}/${token.id}`);
-      const counts = new Array(25).fill(0);
-      let takes = 0;
-      res.data.swing_distribution.forEach((l: SwingLocation) =>
-        l.swing_type === 2
-          ? (takes += l.count)
-          : (counts[l.swing_vertical * 5 + l.swing_horizontal] = l.count),
-      );
-      const total = counts.reduce((acc, value) => acc + value);
-      const rates = counts.map((value) => value / total);
-      return { rates, counts, takes };
+      try {
+        const res = await axios.get(`${API_URL}/${token.address}/${token.id}`);
+        const counts = new Array(25).fill(0);
+        let takes = 0;
+        res.data.swing_distribution.forEach((l: SwingLocation) =>
+          l.swing_type === 2
+            ? (takes += l.count)
+            : (counts[l.swing_vertical * 5 + l.swing_horizontal] = l.count),
+        );
+        const total = counts.reduce((acc, value) => acc + value);
+        const rates = counts.map((value) => value / total);
+        return { rates, counts, takes };
+      } catch (e) {
+        console.log({ token, e });
+        return;
+      }
     },
     {
       enabled: !!token && !isPitcher,
@@ -60,12 +70,6 @@ const TokenToPlay = ({
   );
   return (
     <div className={styles.container}>
-      {isPitcher && pitchDistributions.data && (
-        <div className={styles.heatMapContainer}>
-          <HeatMapSmall rates={pitchDistributions.data.rates} />
-        </div>
-      )}
-
       <Image
         src={token.image}
         alt={""}
@@ -73,6 +77,11 @@ const TokenToPlay = ({
         width={onClick ? "130" : "100"}
       />
       <div className={styles.content}>
+        {isPitcher && pitchDistributions.data && (
+          <div className={styles.heatMapContainer}>
+            <HeatMapSmall rates={pitchDistributions.data.rates} />
+          </div>
+        )}
         {!isPitcher && swingDistributions.data && (
           <div className={styles.heatMapContainer}>
             <HeatMapSmall rates={swingDistributions.data.rates} />
