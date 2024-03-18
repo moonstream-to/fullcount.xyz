c48fc2643f6fce685a0c28348f1a4268659dda01
Anton Mushnin <anton.mushnin@gmail.com>
2024-03-18 13:06:10 +0300

    Heat map variant for play view

--- web/src/components/HomePage/HeatMapSmall.tsx
+++ web/src/components/HomePage/HeatMapSmall.tsx
@@ -1,13 +1,13 @@
-import React, { useState } from "react";
-import { Box, Flex, Grid, Text } from "@chakra-ui/react";
+import React from "react";
+import { Box, Flex, Grid } from "@chakra-ui/react";
 import { valueToColor } from "../../utils/colors";
 
-const HeatMapSmall = ({ rates }: { rates: number[] }) => {
+const HeatMapSmall = ({ rates, size }: { rates: number[]; size?: string }) => {
   const generateCell = (index: number) => (
     <Box key={index}>
       <Box
-        height={{ base: "4px", lg: "4px" }}
-        width={{ base: "4px", lg: "4px" }}
+        height={size ?? "4px"}
+        width={size ?? "4px"}
         display="flex"
         alignItems="center"
         justifyContent="center"
@@ -19,7 +19,11 @@ const HeatMapSmall = ({ rates }: { rates: number[] }) => {
 
   return (
     <Flex direction={"column"} alignItems={"center"} gap={"10px"} minH={{ base: "", lg: "150px" }}>
-      <Grid templateColumns="repeat(5, 1fr)" w={"fit-content"} border={"2px solid #262019"}>
+      <Grid
+        templateColumns="repeat(5, 1fr)"
+        w={"fit-content"}
+        border={size === "10px" ? "1px solid #262019" : "2px solid #262019"}
+      >
         {Array.from({ length: 25 }).map((_, i) => generateCell(i))}
       </Grid>
     </Flex>
