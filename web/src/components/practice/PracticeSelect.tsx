bfe7dd8be4849838907115fec4b29c1d77cc0cd8
Anton Mushnin <anton.mushnin@gmail.com>
2024-03-19 22:17:37 +0300

    joining practice, exit selection

--- web/src/components/practice/PracticeSelect.tsx
+++ web/src/components/practice/PracticeSelect.tsx
@@ -1,15 +1,23 @@
 import styles from "./PracticeView.module.css";
 import parentStyles from "../HomePage/PvpView.module.css";
 import { useState } from "react";
-import { AtBat } from "../../types";
+import { AtBat, OwnedToken, Token } from "../../types";
 import { useGameContext } from "../../contexts/GameContext";
 import TokenToPlay from "../HomePage/TokenToPlay";
+import { useMutation, useQueryClient } from "react-query";
+import { joinSessionFullcountPlayer } from "../../tokenInterfaces/FullcountPlayerAPI";
+import router from "next/router";
+import useUser from "../../contexts/UserContext";
+import { Image, Spinner, useToast } from "@chakra-ui/react";
+import useMoonToast from "../../hooks/useMoonToast";
+import { FULLCOUNT_ASSETS_PATH } from "../../constants";
+import ExitIcon from "../icons/ExitIcon";
 
 const levels = ["Easy", "Medium", "Hard"];
 
 const PracticeSelect = () => {
   const [selectedLevel, setSelectedLevel] = useState(0);
-  const { atBatsForPractice } = useGameContext();
+  const { atBatsForPractice, selectedToken } = useGameContext();
 
   const getSelectedToken = (atBat: AtBat | undefined) => {
     if (atBat?.pitcher) return { token: atBat.pitcher, isPitcher: true };
@@ -17,11 +25,103 @@ const PracticeSelect = () => {
     return { token: undefined, isPitcher: false };
   };
 
+  const queryClient = useQueryClient();
+  const { user } = useUser();
+  const toast = useMoonToast();
+  const joinSession = useMutation(
+    async ({
+      sessionID,
+      token,
+      inviteCode,
+    }: {
+      sessionID: number;
+      token: OwnedToken;
+      inviteCode: string;
+    }): Promise<unknown> => {
+      return joinSessionFullcountPlayer({ token, sessionID, inviteCode });
+    },
+    {
+      onSuccess: async (data, variables) => {
+        let atBatId: number | undefined = undefined;
+        queryClient.setQueryData(
+          ["atBats"],
+          (oldData: { atBats: AtBat[]; tokens: Token[] } | undefined) => {
+            console.log(oldData);
+            if (!oldData) {
+              return { atBats: [], tokens: [] };
+            }
+            const newAtBats = oldData.atBats.map((atBat) => {
+              if (atBat.lastSessionId !== variables.sessionID) {
+                return atBat;
+              }
+              atBatId = atBat.id;
+              if (!atBat.pitcher) {
+                return { ...atBat, progress: 3, pitcher: { ...variables.token } };
+              }
+              if (!atBat.batter) {
+                return { ...atBat, progress: 3, batter: { ...variables.token } };
+              }
+              return atBat;
+            });
+
+            return { atBats: newAtBats, tokens: oldData.tokens };
+          },
+        );
+        queryClient.setQueryData(["owned_tokens", user], (oldData: OwnedToken[] | undefined) => {
+          console.log(oldData);
+          if (!oldData) {
+            return [];
+          }
+          return oldData.map((t) => {
+            if (t.address === variables.token.address && t.id === variables.token.id) {
+              return {
+                ...t,
+                isStaked: true,
+                stakedSessionID: variables.sessionID,
+                tokenProgress: 3,
+              };
+            }
+            return t;
+          });
+        });
+        queryClient.invalidateQueries("owned_tokens");
+        if (atBatId) {
+          router.push(`atbats/?id=${atBatId}`);
+        }
+      },
+      onError: (e: Error) => {
+        toast("Join failed" + e?.message, "error");
+      },
+    },
+  );
+
+  const handlePlay = (atBat: AtBat | undefined) => {
+    if (selectedToken && atBat) {
+      joinSession.mutate({
+        sessionID: atBat.lastSessionId ?? 0,
+        token: selectedToken,
+        inviteCode: "",
+      });
+    }
+  };
+
   if (!atBatsForPractice) {
     return <></>;
   }
   return (
     <div className={styles.container}>
+      <div className={styles.exitButton} onClick={() => router.push("/")}>
+        <ExitIcon onClick={() => router.push("/")} />
+      </div>
+      <Image
+        minW={"552px"}
+        h={"123px"}
+        position={"absolute"}
+        src={`${FULLCOUNT_ASSETS_PATH}/stadium.png`}
+        right={"50%"}
+        bottom={"50%"}
+        transform={"translateX(50%) translateY(50%)"}
+      />
       {getSelectedToken(atBatsForPractice[selectedLevel]).token && (
         <TokenToPlay
           token={getSelectedToken(atBatsForPractice[selectedLevel]).token}
@@ -39,7 +139,18 @@ const PracticeSelect = () => {
           </div>
         ))}
       </div>
-      <div className={styles.actionButton}>Train</div>
+      <div
+        className={styles.actionButton}
+        onClick={() => handlePlay(atBatsForPractice[selectedLevel])}
+      >
+        {joinSession.isLoading ? (
+          <Spinner h={4} w={4} />
+        ) : joinSession.isSuccess ? (
+          "Success"
+        ) : (
+          "Train"
+        )}
+      </div>
     </div>
   );
 };
