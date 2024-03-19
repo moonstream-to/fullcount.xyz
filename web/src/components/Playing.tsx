0e220af2b3991c4a0e8be1b83a53d22efa2f0489
Anton Mushnin <anton.mushnin@gmail.com>
2024-03-19 22:33:25 +0300

    tokens loading view

--- web/src/components/Playing.tsx
+++ web/src/components/Playing.tsx
@@ -1,4 +1,4 @@
-import { Flex } from "@chakra-ui/react";
+import { Image } from "@chakra-ui/react";
 
 import { useGameContext } from "../contexts/GameContext";
 import SessionsView from "./sessions/SessionsView";
@@ -15,6 +15,8 @@ import PlayingLayout from "./layout/PlayingLayout";
 import ChooseToken from "./tokens/ChooseToken";
 import HomePage from "./HomePage/HomePage";
 import { getAtBats } from "../services/fullcounts";
+import React, { useEffect } from "react";
+import { FULLCOUNT_ASSETS_PATH } from "../constants";
 
 const Playing = () => {
   const {
@@ -57,6 +59,57 @@ const Playing = () => {
       },
     },
   );
+
+  if (!atBats.data || !ownedTokens.data) {
+    return (
+      <div className={styles.loadingViewContainer}>
+        <Image
+          alt={""}
+          minW={"552px"}
+          h={"123px"}
+          position={"absolute"}
+          src={`${FULLCOUNT_ASSETS_PATH}/stadium.png`}
+          right={"50%"}
+          bottom={"75%"}
+          transform={"translateX(50%) translateY(50%)"}
+          filter={"blur(0px)"}
+        />
+        <Image
+          src={`${FULLCOUNT_ASSETS_PATH}/logo-4-no-stroke.png`}
+          position={"absolute"}
+          top={"10%"}
+          right={"50%"}
+          w={"158px"}
+          transform={"translateX(50%) translateY(-40px)"}
+          alt={""}
+          h={"84px"}
+          zIndex={"0"}
+        />
+        <Image
+          src={`${FULLCOUNT_ASSETS_PATH}/batter.png`}
+          position={"absolute"}
+          bottom={"0"}
+          right={"50%"}
+          minW={"165px"}
+          transform={"translateX(0) translateY(-40px)"}
+          alt={""}
+          h={"395px"}
+          zIndex={"0"}
+        />
+        <Image
+          src={`${FULLCOUNT_ASSETS_PATH}/pitcher.png`}
+          position={"absolute"}
+          bottom={"65%"}
+          right={"50%"}
+          transform={"translateX(50%) translateY(100%)"}
+          alt={""}
+          w={"60px"}
+          h={"80px"}
+          zIndex={"0"}
+        />
+      </div>
+    );
+  }
 
   return (
     <>
