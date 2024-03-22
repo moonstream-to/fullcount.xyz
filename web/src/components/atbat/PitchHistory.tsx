f9c4903bd53de43c9e2def9d1b492f618caacae6
Anton Mushnin <anton.mushnin@gmail.com>
2024-03-22 17:25:34 +0300

    pitches history

---
+++ web/src/components/atbat/PitchHistory.tsx
@@ -0,0 +1,66 @@
+import styles from "./PitchHistory.module.css";
+import { AtBat, AtBatStatus, SessionStatus } from "../../types";
+import BallIcon from "../icons/BallIcon";
+import HistoryGrid from "./HistoryGrid";
+import BatIcon from "../icons/BatIcon";
+import { useGameContext } from "../../contexts/GameContext";
+import { sessionOutcomeType } from "./Outcome2";
+import { sessionOutcomes } from "./AtBatView";
+
+const PitchHistory = ({ atBat }: { atBat: AtBatStatus }) => {
+  const { selectedToken } = useGameContext();
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <div className={styles.title}>PITCH HISTORY</div>
+        <div className={styles.total}>{`${atBat.pitches.length} PITCHES`}</div>
+      </div>
+      <div className={styles.list}>
+        {atBat.pitches
+          .filter((p) => p.progress === 5)
+          .map((p, idx) => (
+            <div className={styles.item} key={idx}>
+              <div className={styles.itemText}>{idx + 1}</div>
+              <div className={styles.info}>
+                <BallIcon />
+                <HistoryGrid
+                  vertical={p.pitcherReveal.vertical}
+                  horizontal={p.pitcherReveal.horizontal}
+                />
+                <div className={styles.itemText}>
+                  {p.pitcherReveal.speed === "0" ? "FAST" : "SLOW"}
+                </div>
+                <div className={styles.divider} />
+                <HistoryGrid
+                  vertical={p.batterReveal.kind === "2" ? "-1" : p.batterReveal.vertical}
+                  horizontal={p.batterReveal.kind === "2" ? "-1" : p.batterReveal.horizontal}
+                />
+                <div className={styles.itemText}>
+                  {p.batterReveal.kind === "0"
+                    ? "CONTACT"
+                    : p.batterReveal.kind === "1"
+                    ? "POWER"
+                    : "TAKE"}
+                </div>
+                <BatIcon />
+              </div>
+              {selectedToken && (
+                <div
+                  className={
+                    sessionOutcomeType([selectedToken], atBat, p) === "positive"
+                      ? styles.positiveOutcome
+                      : styles.negativeOutcome
+                  }
+                >
+                  {sessionOutcomes[p.outcome]}
+                </div>
+              )}
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default PitchHistory;
