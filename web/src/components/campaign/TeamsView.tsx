fd353680bc39b5738b149067977adef1b5499c08
Anton Mushnin <anton.mushnin@gmail.com>
2024-03-25 13:39:11 +0300

    filter campaign atBats, atBats offset

--- web/src/components/campaign/TeamsView.tsx
+++ web/src/components/campaign/TeamsView.tsx
@@ -12,16 +12,18 @@ const TeamsView = ({ atBats, isPitching }: { atBats: AtBat[]; isPitching: boolea
         <TeamView
           key={idx}
           isPitching={isPitching}
-          atBats={atBats.filter((atBat) =>
-            team.roster.some(
-              (token) =>
-                token.isPitcher === isPitching &&
-                token.tokens.some(
-                  (t) =>
-                    (t.address === atBat.pitcher?.address && t.id === atBat.pitcher.id) ||
-                    (t.address === atBat.batter?.address && t.id === atBat.batter.id),
-                ),
-            ),
+          atBats={atBats.filter(
+            (atBat) =>
+              atBat.progress === 2 &&
+              team.roster.some(
+                (token) =>
+                  token.isPitcher === isPitching &&
+                  token.tokens.some(
+                    (t) =>
+                      (t.address === atBat.pitcher?.address && t.id === atBat.pitcher.id) ||
+                      (t.address === atBat.batter?.address && t.id === atBat.batter.id),
+                  ),
+              ),
           )}
           team={team}
         />
