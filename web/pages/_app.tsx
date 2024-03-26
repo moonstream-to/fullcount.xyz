1abc29d4c0518574bae8063269b33eb49103ed49
Anton Mushnin <anton.mushnin@gmail.com>
2024-03-26 15:28:05 +0300

    script

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -9,12 +9,24 @@ import { Web3Context } from "../src/contexts";
 import "../src/styles/globals.css";
 import { GameContextProvider } from "../src/contexts/GameContext";
 import { UserProvider } from "../src/contexts/UserContext";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(new QueryClient());
 
   return (
     <>
+      <Script
+        id="google-tag-manager"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','GTM-KSQM8K8K');`,
+        }}
+      />
       <ChakraProvider theme={theme}>
         <QueryClientProvider client={queryClient}>
           <Web3Context>
