8d871cc8b9fe604a0480206f102e8d9a18867263
Anton Mushnin <anton.mushnin@gmail.com>
2024-04-01 13:38:46 +0300

    display only needed wins

--- web/src/components/campaign/CharacterProgress.tsx
+++ web/src/components/campaign/CharacterProgress.tsx
@@ -11,15 +11,15 @@ const CharacterProgress = ({
     <div className={styles.tokenStatItem} style={{ borderColor: color }}>
       <div className={styles.statHeader}>
         <div className={styles.label}>{stat.label}</div>
-        <div className={styles.completion}>{`${stat.finished}${stat.total ? "/" : ""}${
-          stat.total ? stat.total : ""
-        }`}</div>
+        <div className={styles.completion}>{`${Math.min(stat.finished, stat.total)}${
+          stat.total ? "/" : ""
+        }${stat.total ? stat.total : ""}`}</div>
       </div>
       {stat.total && (
         <div className={styles.notFinishedBar} style={{ borderColor: color }}>
           <div
             className={styles.finishedBar}
-            style={{ width: `${(100 * stat.finished) / stat.total}%` }}
+            style={{ width: `${(100 * Math.min(stat.finished, stat.total)) / stat.total}%` }}
           />
         </div>
       )}
