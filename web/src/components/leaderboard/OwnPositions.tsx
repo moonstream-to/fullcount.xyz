51a870efa05656b77e5485cdb73ad73c8bd71492
Anton Mushnin <anton.mushnin@gmail.com>
2024-04-17 15:11:28 +0300

    animating own positions

--- web/src/components/leaderboard/OwnPositions.tsx
+++ web/src/components/leaderboard/OwnPositions.tsx
@@ -6,15 +6,39 @@ import { useGameContext } from "../../contexts/GameContext";
 import LeaderboardHeader from "./LeaderboardHeader";
 import LeaderboardItem from "./LeaderboardItem";
 
-const OwnPositions = ({ tokens, leaderboardId }: { tokens: Token[]; leaderboardId: string }) => {
+const OwnPositions = ({
+  tokens,
+  leaderboardId,
+}: {
+  tokens: Token[] | undefined;
+  leaderboardId: string;
+}) => {
   const { tokensCache } = useGameContext();
 
-  const ownPositions = useQuery(["own_leaderboard_positions", tokens, leaderboardId], async () => {
-    return fetchPositionForTokens(leaderboardId, tokens, tokensCache);
-  });
+  const ownPositions = useQuery(
+    ["own_leaderboard_positions", tokens, leaderboardId],
+    async () => {
+      if (!tokens) {
+        return [];
+      }
+      return fetchPositionForTokens(leaderboardId, tokens, tokensCache);
+    },
+    {
+      enabled: !!tokens,
+    },
+  );
 
   return (
-    <div className={styles.container}>
+    <div
+      className={styles.container}
+      style={{
+        height: ownPositions.data
+          ? `${52 + ownPositions.data.length * 39}px`
+          : tokens
+          ? `${52 + tokens.length * 39}px`
+          : "52px",
+      }}
+    >
       <LeaderboardHeader />
       {ownPositions.data &&
         ownPositions.data.map((entry, idx) => <LeaderboardItem entry={entry} key={idx} />)}
