7aa2211ce41ae6d9e34284d53891701e473d4633
Anton Mushnin <anton.mushnin@gmail.com>
2024-04-18 12:57:33 +0300

    using launch form

--- web/src/components/TitleScreen.tsx
+++ web/src/components/TitleScreen.tsx
@@ -9,9 +9,12 @@ import LoginForm from "./account/LoginForm";
 
 import { FULLCOUNT_ASSETS, FULLCOUNT_ASSETS_PATH } from "../constants";
 import LoadingView from "./HomePage/LoadingView";
+import LaunchForm from "./LaunchForm";
+import MoonstreamLogo2 from "./icons/MoonstreamLogo2";
 const TitleScreen = () => {
   const { user, isLoading } = useUser();
   const [isLogging, setIsLogging] = useState(false); // login or signUp
+  const [isLaunching, setIsLaunching] = useState(true);
   const [isSuccess, setIsSuccess] = useState(false);
   const [isFirstSeconds, setIsFirstSeconds] = useState(true);
 
@@ -29,39 +32,49 @@ const TitleScreen = () => {
             <LoadingView />
           ) : (
             <div className={styles.loginContainer}>
-              {/*<Image*/}
-              {/*  alt={""}*/}
-              {/*  className={styles.logo}*/}
-              {/*  height={"84"}*/}
-              {/*  width={"158"}*/}
-              {/*  priority*/}
-              {/*  src={`${FULLCOUNT_ASSETS_PATH}/logo-4-no-stroke.png`}*/}
-              {/*/>*/}
-              <div className={styles.content}>
+              <div className={styles.content} style={{ maxWidth: isLaunching ? "320px" : "400px" }}>
                 <img
                   className={styles.banner}
                   src={`${FULLCOUNT_ASSETS}/landing/banner.png`}
                   alt={""}
                 />
-
-                {!isLogging ? (
-                  <SignUpForm setIsSuccess={(value) => setIsSuccess(value)} />
+                {isLaunching ? (
+                  <LaunchForm onClose={() => setIsLaunching(false)} />
                 ) : (
-                  <LoginForm setIsSuccess={(value) => setIsSuccess(value)} />
+                  <>
+                    {!isLogging ? (
+                      <SignUpForm setIsSuccess={(value) => setIsSuccess(value)} />
+                    ) : (
+                      <LoginForm setIsSuccess={(value) => setIsSuccess(value)} />
+                    )}
+                    {!isSuccess && (
+                      <div className={styles.footer}>
+                        <div className={styles.footerText}>
+                          {isLogging ? "New to Fullcount?" : "Already have an account?"}
+                        </div>
+                        <div
+                          className={styles.footerButton}
+                          onClick={() => setIsLogging((prev) => !prev)}
+                        >
+                          {isLogging ? "Create an account" : "Log in"}
+                        </div>
+                      </div>
+                    )}
+                  </>
                 )}
-                {!isSuccess && (
-                  <div className={styles.footer}>
-                    <div className={styles.footerText}>
-                      {isLogging ? "New to Fullcount?" : "Already have an account?"}
-                    </div>
-                    <div
-                      className={styles.footerButton}
-                      onClick={() => setIsLogging((prev) => !prev)}
-                    >
-                      {isLogging ? "Create an account" : "Log in"}
-                    </div>
+                <div
+                  className={styles.contentFooter}
+                  style={{ marginTop: isLaunching ? "-20px" : "0" }}
+                >
+                  <div className={styles.builtBy}>
+                    {"built by"}
+                    <MoonstreamLogo2 />
                   </div>
-                )}
+                  <div className={styles.builtBy}>
+                    {"powered by"}
+                    <img alt={"nova"} src={`${FULLCOUNT_ASSETS}/icons/arbitrum-nova-logo.svg`} />
+                  </div>
+                </div>
               </div>
             </div>
           )}
