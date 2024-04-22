192965231795558e1d49e694d66342ca58517ec8
Anton Mushnin <anton.mushnin@gmail.com>
2024-04-22 17:47:35 +0300

    fetching tokens positions with windows

--- web/src/components/leaderboard/leaderboards.ts
+++ web/src/components/leaderboard/leaderboards.ts
@@ -31,11 +31,11 @@ export async function fetchLeaderboardData(
   });
 }
 
-export async function fetchPositionForTokens(
+export const fetchPositionForTokens = async (
   leaderboardId: string,
   tokens: TokenId[],
   tokensCache: Token[],
-): Promise<LeaderboardEntry[]> {
+): Promise<LeaderboardEntry[]> => {
   const promises = tokens.map(async (t) => {
     const res = await axios.get(
       `https://engineapi.moonstream.to/leaderboard/position/?leaderboard_id=${leaderboardId}&address=${t.address}_${t.id}&normalize_addresses=false&window_size=0`,
@@ -56,4 +56,31 @@ export async function fetchPositionForTokens(
     const metadata = tokensMetadata.find((t) => t.id === entry.id && t.address === entry.address);
     return { ...entry, image: metadata?.image ?? "", name: metadata?.name ?? "" };
   });
-}
+};
+
+export const fetchWindowsForTokens = async (
+  leaderboardId: string,
+  tokens: TokenId[],
+  tokensCache: Token[],
+  windowSize: number,
+): Promise<LeaderboardEntry[]> => {
+  const promises = tokens.map(async (t) => {
+    const res = await axios.get(
+      `https://engineapi.moonstream.to/leaderboard/position/?leaderboard_id=${leaderboardId}&address=${
+        t.address
+      }_${t.id}&normalize_addresses=false&window_size=${windowSize ?? 0}`,
+    );
+    return res.data;
+  });
+  const response = await Promise.all(promises);
+
+  const entries: any = response.map((entry: { address: string }) => {
+    const [address, id] = entry.address.split("_");
+    return { ...entry, address, id };
+  });
+  const tokensMetadata = await getTokensMetadata(entries, tokensCache);
+  return entries.map((entry: Token) => {
+    const metadata = tokensMetadata.find((t) => t.id === entry.id && t.address === entry.address);
+    return { ...entry, image: metadata?.image ?? "", name: metadata?.name ?? "" };
+  });
+};
