818242a37dcf67216f2695ff77393e4df9264a04
Anton Mushnin <anton.mushnin@gmail.com>
2024-04-23 13:58:49 +0300

    desktop view

--- web/src/components/leaderboard/LeaderboardLayout.tsx
+++ web/src/components/leaderboard/LeaderboardLayout.tsx
@@ -1,16 +1,22 @@
 import styles from "./LeaderboardLayout.module.css";
 import Navbar from "../layout/Navbar";
 import { ReactNode } from "react";
+import { useMediaQuery } from "@chakra-ui/react";
 
 const LeaderboardLayout = ({ children }: { children: ReactNode }) => {
+  const [isWideView] = useMediaQuery(["(min-width: 768px)"]);
   return (
     <div className={styles.container}>
       <div className={styles.navbarContainer}>
         <Navbar />
       </div>
       <img
-        src="https://static.fullcount.xyz/web/banners/leaderboards-banner-2-sm.png"
-        alt="Description of the image content"
+        src={
+          isWideView
+            ? "https://static.fullcount.xyz/web/banners/leaderboards-banner-2.jpg"
+            : "https://static.fullcount.xyz/web/banners/leaderboards-banner-2-sm.png"
+        }
+        alt=""
       />
       {children}
     </div>
