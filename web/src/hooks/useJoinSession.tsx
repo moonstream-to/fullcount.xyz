612d8c01443046a5250dcfd6de3154274214070a
Anton Mushnin <anton.mushnin@gmail.com>
2024-05-15 15:07:21 +0300

    useJoin hook

---
+++ web/src/hooks/useJoinSession.tsx
@@ -0,0 +1,86 @@
+import { useMutation, useQueryClient } from "react-query";
+import { useRouter } from "next/router";
+import { AtBat, OwnedToken, Token } from "../types";
+import { joinSessionFullcountPlayer } from "../tokenInterfaces/FullcountPlayerAPI";
+import useMoonToast from "./useMoonToast";
+import { sendReport } from "../utils/humbug";
+import useUser from "../contexts/UserContext";
+
+type JoinSessionParams = {
+  sessionID: number;
+  token: OwnedToken;
+  inviteCode: string;
+};
+
+export const useJoinSession = () => {
+  const queryClient = useQueryClient();
+  const router = useRouter();
+  const toast = useMoonToast();
+  const user = useUser();
+
+  return useMutation(
+    async ({ sessionID, token, inviteCode }: JoinSessionParams): Promise<unknown> => {
+      return joinSessionFullcountPlayer({ token, sessionID, inviteCode });
+    },
+    {
+      onSuccess: async (data, variables) => {
+        let atBatId: number | undefined = undefined;
+        queryClient.setQueryData(
+          ["atBats"],
+          (oldData: { atBats: AtBat[]; tokens: Token[] } | undefined) => {
+            if (!oldData) {
+              return { atBats: [], tokens: [] };
+            }
+            const newAtBats = oldData.atBats.map((atBat) => {
+              if (atBat.lastSessionId !== variables.sessionID) {
+                return atBat;
+              }
+              atBatId = atBat.id;
+              if (!atBat.pitcher) {
+                return { ...atBat, progress: 3, pitcher: { ...variables.token } };
+              }
+              if (!atBat.batter) {
+                return { ...atBat, progress: 3, batter: { ...variables.token } };
+              }
+              return atBat;
+            });
+
+            return { atBats: newAtBats, tokens: oldData.tokens };
+          },
+        );
+        queryClient.setQueryData(["owned_tokens", user], (oldData: OwnedToken[] | undefined) => {
+          if (!oldData) {
+            return [];
+          }
+          return oldData.map((t) => {
+            if (t.address === variables.token.address && t.id === variables.token.id) {
+              return {
+                ...t,
+                isStaked: true,
+                stakedSessionID: variables.sessionID,
+                tokenProgress: 3,
+              };
+            }
+            return t;
+          });
+        });
+        queryClient.invalidateQueries("owned_tokens");
+        if (atBatId) {
+          router.push(`atbats/?id=${atBatId}`);
+        }
+      },
+      retryDelay: (attemptIndex) => (attemptIndex < 1 ? 5000 : 10000),
+      retry: (failureCount, error) => {
+        console.log(error);
+        if (failureCount < 3) {
+          console.log("Will retry in 5, maybe 10 seconds");
+        }
+        return failureCount < 3;
+      },
+      onError: (e: Error) => {
+        toast("Join failed" + e?.message, "error");
+        sendReport("Error toast", { error: e }, ["type:error_toast"]);
+      },
+    },
+  );
+};
