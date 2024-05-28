70a23160d28c55e44554bc9c29d755a7a927be9a
Anton Mushnin <anton.mushnin@gmail.com>
2024-05-28 18:48:24 +0300

    join, abort, token state

--- web/src/tokenInterfaces/TrustedExecutorAPI.ts
+++ web/src/tokenInterfaces/TrustedExecutorAPI.ts
@@ -1,31 +1,34 @@
-import { AtBatStatus, Token, TokenId } from "../types";
+import { AtBatStatus, Token, TokenId, TrustedExecutionAtBatState } from "../types";
 import { FULLCOUNT_PLAYER_API, GAME_CONTRACT, ZERO_ADDRESS } from "../constants";
 import axios from "axios";
 import { getContracts } from "../utils/getWeb3Contracts";
 import { sendReport } from "../utils/humbug";
 import { delay, getHeaders, unstakeFullcountPlayer } from "./FullcountPlayerAPI";
 import { getTokensData } from "./BLBTokenAPI";
+import { emptyPitch } from "../components/atbat/OnboardingAPI";
+import atBatView from "../components/atbat/AtBatView";
 
 export async function startSessionTrustedExecutor({
   token,
-  roleNumber,
+  role,
   requireSignature,
 }: {
   token: Token;
-  roleNumber: number;
+  role: 0 | 1;
   requireSignature: boolean;
-}): Promise<{ sessionID: string; sign: string | undefined }> {
+}): Promise<{ sessionID: undefined; atBatID: string; inviteCode: string | undefined }> {
   const postData = {
     nft: { erc721_address: token.address, token_id: token.id },
+    role,
+    requireSignature,
   };
   const headers = getHeaders();
-  // await unstakeFullcountPlayer({ token });
 
-  const data = await axios
+  const state = await axios
     .post(`${FULLCOUNT_PLAYER_API}/trusted/start`, postData, { headers })
     .then(async (response) => {
       console.log("Success:", response.data);
-      return response.data;
+      return response.data.state;
     });
   sendReport("Session started", {}, [
     "type:contract",
@@ -33,7 +36,7 @@ export async function startSessionTrustedExecutor({
     `token_address:${token.address}`,
     `token_id:${token.id}`,
   ]);
-  return { sessionID: data.session_id, sign: data.signature };
+  return { atBatID: state.at_bat_id, inviteCode: undefined, sessionID: undefined };
 }
 
 export const getAtBatTrustedExecutor = async (id: string, tokensCache: Token[]) => {
@@ -69,7 +72,7 @@ export const getAtBatTrustedExecutor = async (id: string, tokensCache: Token[]) 
   const tokens = [...tokensCache, ...newTokensData];
   const atBat: AtBatStatus = {
     pitcher: tokens.find(
-      (t) => t.address === state.batter_nft.erc721_address && t.id === state.batter_nft.token_id,
+      (t) => t.address === state.pitcher_nft.erc721_address && t.id === state.pitcher_nft.token_id,
     ),
     batter: tokens.find(
       (t) => t.address === state.batter_nft.erc721_address && t.id === state.batter_nft.token_id,
@@ -77,9 +80,85 @@ export const getAtBatTrustedExecutor = async (id: string, tokensCache: Token[]) 
     balls: state.balls,
     strikes: state.strikes,
     outcome: state.outcome,
-    pitches: [],
+    pitches: state.current_session_index === 1 ? [{ ...emptyPitch, progress: 2 }] : [],
     numberOfSessions: state.current_session_index,
     id,
   };
   return { atBat, tokens };
+};
+
+export const joinAtBatTrustedExecutor = async ({
+  token,
+  atBatID,
+}: {
+  token: Token;
+  atBatID: string;
+}) => {
+  const postData = {
+    nft: { erc721_address: token.address, token_id: token.id },
+    at_bat_id: atBatID,
+  };
+  const headers = getHeaders();
+  // await unstakeFullcountPlayer({ token });
+
+  const data = await axios
+    .post(`${FULLCOUNT_PLAYER_API}/trusted/join`, postData, { headers })
+    .then(async (response) => {
+      console.log("Success:", response.data);
+      return response.data;
+    });
+  sendReport("Session started", {}, [
+    "type:contract",
+    "contract_method:start",
+    `token_address:${token.address}`,
+    `token_id:${token.id}`,
+  ]);
+  return data;
+};
+
+export const abortAtBatTrustedExecutor = async ({
+  token,
+  atBatID,
+}: {
+  token: Token;
+  atBatID: string;
+}) => {
+  const postData = {
+    nft: { erc721_address: token.address, token_id: token.id },
+    at_bat_id: atBatID,
+  };
+  const headers = getHeaders();
+  // await unstakeFullcountPlayer({ token });
+
+  const data = await axios
+    .post(`${FULLCOUNT_PLAYER_API}/trusted/abort`, postData, { headers })
+    .then(async (response) => {
+      console.log("Success:", response.data);
+      return response.data;
+    });
+  sendReport("Session started", {}, [
+    "type:contract",
+    "contract_method:start",
+    `token_address:${token.address}`,
+    `token_id:${token.id}`,
+  ]);
+  return data;
+};
+
+const fetchCurrentAtBat = (token: Token): Promise<TrustedExecutionAtBatState> => {
+  const headers = getHeaders();
+  return axios
+    .get(
+      `${FULLCOUNT_PLAYER_API}/trusted/current?token_address=${token.address}&token_id=${token.id}`,
+      { headers },
+    )
+    .then((res) => res.data.state);
+};
+
+export const fetchAllCurrentAtBats = async (
+  tokens: Token[],
+): Promise<(TrustedExecutionAtBatState | undefined)[]> => {
+  const promises = tokens.map((token) => fetchCurrentAtBat(token));
+  const results = await Promise.allSettled(promises);
+  return results.map((result) => (result.status === "fulfilled" ? result.value : undefined));
 };
